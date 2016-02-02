03be101d917fb52dcc49bf5ca3bb21126cb4274c
Arno den Uijl <aduijl@sogyo.nl>
2016-02-02 20:16:03 +0100

    create person with real input

--- src/components/editPerson.ts
+++ src/components/editPerson.ts
@@ -5,40 +5,58 @@ import {Observable} from "rx";
 import {Person} from "../person";
 import {AddPerson, DeletePersons} from "../personStoreDriver";
 
-export function EditPerson(drivers: { DOM: any, PersonStoreDriver: Observable<Person[]>  }) {
-  var personCountChanged$ = drivers.PersonStoreDriver.do(x => console.log("Person array updated " + x.length))
-                      .map(persons => persons.length)
-                      .distinctUntilChanged()
-                      .bufferWithCount(2, 1)
-                      .map(personLengths => personLengths[1] - personLengths[0])
-                      .share();
+export function EditPerson(drivers: { DOM: any, PersonStoreDriver: Observable<Person[]> }) {
+    let personCountChanged$ = drivers.PersonStoreDriver.do(x => console.log("Person array updated " + x.length))
+        .map(persons => persons.length)
+        .distinctUntilChanged()
+        .bufferWithCount(2, 1)
+        .map(personLengths => personLengths[1] - personLengths[0])
+        .share();
 
-  let changedMessage$ = personCountChanged$.map(changedNum => changedNum > 0 ?  div("Added " + changedNum + " persons") : div("Deleted " + changedNum + " persons"))
-                                            .do(x => console.log("changed"));
-  let clearMessage$ = changedMessage$.flatMap(_ => Observable.timer(2000)).map(x => "").do(x => console.log("cleared"));
+    let personCountChangedMessage$ = personCountChanged$.map(changedNum => changedNum > 0 ? div("Added " + changedNum + " persons") : div("Deleted " + changedNum + " persons"))
+        .do(x => console.log("changed"));
+    let clearMessage$ = personCountChangedMessage$.flatMap(_ => Observable.timer(2000)).map(x => "").do(x => console.log("cleared"));
 
-  let message$ = Observable.merge(changedMessage$, clearMessage$).startWith("")
-
-  let dummyUsers: Person[] = [
-        { id: 0, firstName: "Arno", lastName: "den Uijl" },
-      ];
+    let message$ = Observable.merge(personCountChangedMessage$, clearMessage$).startWith("")
 
     let createClick$: Observable<MouseEvent> = drivers.DOM.select(".create-default").events("click");  // Observe de delete click events
+    let firstNameInput$: Observable<any> = drivers.DOM.select(".firstName").events("input").map(ev => ev.target.value);  // Observe de delete click events
+    let lastNameInput$: Observable<any> = drivers.DOM.select(".lastName").events("input").map(ev => ev.target.value);  // Observe de delete click events
 
-    let storageRequests = createClick$.flatMap(_ => Observable.fromArray(dummyUsers))
-                                       .map(x => new AddPerson(x.firstName, x.lastName));
+    let storageRequest$ = Observable.combineLatest(firstNameInput$, lastNameInput$, (f, l) => ({firstName: f, lastName: l}))
+                .sample(createClick$)
+                .filter(x => x.firstName !== "" &&  x.lastName !== "")
+                .map(x => new AddPerson(x.firstName, x.lastName));
 
     let vtree$ = message$.do(x => console.log("message changed"))
         .map(message =>                                         // create the DOM from the array
             div([
-                div([
-                    button(".create-default", "create default")
-                ]),
-                message
+                div(".container", [
+                    div(".row", [
+                        div(".two .columns", [
+                            label(".label", "First name"),
+                        ]),
+                        div(".three .columns", [
+                            input(".firstName")
+                        ])
+                    ]),
+                    div(".row", [
+                        div(".two .columns", [
+                            label(".label", "Last name"),
+                        ]),
+                        div(".three .columns", [
+                            input(".lastName")
+                        ])
+                    ]),
+                    div([
+                        button(".create-default", "Save")
+                    ]),
+                    div(message)
+                ])
             ]));
 
     return {
         DOM: vtree$,
-        PersonStoreDriver: storageRequests
+        PersonStoreDriver: storageRequest$
     };
 }
