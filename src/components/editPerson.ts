de1399e51e6394e130e5423d65d99444f337cd06
Arno den Uijl <aduijl@sogyo.nl>
2016-02-03 21:18:05 +0100

    comments

--- src/components/editPerson.ts
+++ src/components/editPerson.ts
@@ -5,6 +5,54 @@ import {Observable} from "rx";
 import {Person} from "../person";
 import {AddPerson} from "../personStoreDriver";
 
+/**
+ *            DOM                               PersonStoreDriver
+ *   ______________________________           _____________________
+ *       |             |         |                     |
+ * firstNameInput$     |         |                  persons$
+ *       |             |         |                     |
+ *       |     lastNameInput$    |                     |
+ *       |             |         |                     V
+ *       |             |   createClick$        personCountChanged$
+ *       |             |         |                     |
+ *       |             |         |                     |
+ *       |             |         |                     V
+ *       |_ _ _ _    _ |         |        personCountChangedMessage$
+ *              |   |            |                |           |
+ *              V___V            |                |           |
+ *               |               |                |           |
+ *               | <-------------|                |           V
+ *               |                                |      clearMessage$
+ *               |                                |           |
+ *               |                                |_ _ _ _ _ _|
+ *               |                                      |           
+ *               |                                      V           
+ *          addRequest$                              vtree$
+ *                |                                     |
+ *                |                                     |
+ *                V                                     V
+ *       ____________________                    ________________
+ *        PersonStoreDriver                            DOM
+ *
+ *
+ * DOM                   -> DOM driver
+ *                               -> Source: Virtual DOM from which we can listen to events from the user
+ *                               -> Sink:   Takes an Observable of generated Virtual DOMs
+ * PersonStoreDriver     -> Local storage driver for persons
+ *                               -> Source: Observable that gives a list of Persons everytime the list in the storage is updated
+ *                               -> Sink:   Takes an Observable of actions on the persons in storage to add and delete persons (AddPerson, DeletePersons)
+ *
+ * persons$                    -> Stream of Person[]. Everytime the list of persons changes a new array is pushed
+ * personCountChanged$         -> Listens to the persons$ stream and emits a number indicating how many persons are added or deleted
+ * personCountChangedMessage$  -> Formats a  message for the user about the change of person count
+ * clearMessage$               -> Times an empty message after each personCountChangedMessage$ to clear the message
+ * vtree$                      -> The virtual dom is created from the person$ (for the data) and the selectedIds$ (for the highlights)
+ * firstNameInput$             -> Stream of the inputed first name
+ * lastNameInput$              -> Stream of the inputed last name
+ * createClick$                -> When the user clicks save
+ * addRequest$                 -> Whem the user clicks save and the first and last names are not empty a request is made to persist the person
+ **/
+
 export function EditPerson(drivers: { DOM: any, PersonStoreDriver: Observable<Person[]> }) {
         // Updates from the Person[]
     let persons$ = drivers.PersonStoreDriver.do(x => console.log("persons$: " + x.length + " persons")).shareReplay(1);
@@ -29,12 +77,11 @@ export function EditPerson(drivers: { DOM: any, PersonStoreDriver: Observable<Pe
     let firstNameInput$: Observable<any> = drivers.DOM.select(".firstName").events("input").map(ev => ev.target.value).do(x => console.log("firstNameInput$: " + x));
     let lastNameInput$: Observable<any> = drivers.DOM.select(".lastName").events("input").map(ev => ev.target.value).do(x => console.log("lastNameInput$: " + x));
 
-    let storageRequest$ = Observable.combineLatest(firstNameInput$, lastNameInput$, (f, l) => ({ firstName: f, lastName: l }))
-        // .replay(null, 1)
+    let addRequest$ = Observable.combineLatest(firstNameInput$, lastNameInput$, (f, l) => ({ firstName: f, lastName: l }))
         .sample(createClick$)
         .filter(x => x.firstName !== "" && x.lastName !== "")
         .map(x => new AddPerson(x.firstName, x.lastName))
-        .do(x => console.log("storageRequest$: " + JSON.stringify(x)));
+        .do(x => console.log("addRequest$: " + JSON.stringify(x)));
 
     let vtree$ = message$
         .map(message =>
@@ -64,6 +111,6 @@ export function EditPerson(drivers: { DOM: any, PersonStoreDriver: Observable<Pe
             ])).do(x => console.log("vtree$"));
     return {
         DOM: vtree$,
-        PersonStoreDriver: storageRequest$
+        PersonStoreDriver: addRequest$
     };
 }
