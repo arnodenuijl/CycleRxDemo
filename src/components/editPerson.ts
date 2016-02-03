70872a831f9f26f203f9a61e63f11f6cc31feadd
Arno den Uijl <aduijl@sogyo.nl>
2016-02-03 20:31:43 +0100

    logging

--- src/components/editPerson.ts
+++ src/components/editPerson.ts
@@ -6,29 +6,35 @@ import {Person} from "../person";
 import {AddPerson} from "../personStoreDriver";
 
 export function EditPerson(drivers: { DOM: any, PersonStoreDriver: Observable<Person[]> }) {
-    let personCountChanged$ = drivers.PersonStoreDriver.do(x => console.log("Person array updated " + x.length))
+        // Updates from the Person[]
+    let persons$ = drivers.PersonStoreDriver.do(x => console.log("persons$: " + x.length + " persons")).shareReplay(1);
+
+    let personCountChanged$ = persons$
         .map(persons => persons.length)
         .distinctUntilChanged()
         .bufferWithCount(2, 1)
         .map(personLengths => personLengths[1] - personLengths[0])
+        .do(x => console.log("personCountChanged$: diff: " + x))
         .share();
 
     // notification if the person changed count is changed
     let personCountChangedMessage$ = personCountChanged$.map(changedNum => changedNum > 0 ? "Added " + changedNum + " persons" : "Deleted " + changedNum + " persons")
-    let clearMessage$ = personCountChangedMessage$.flatMap(_ => Observable.timer(2000)).map(x => "").do(x => console.log("cleared"));
-    let message$ = Observable.merge(personCountChangedMessage$, clearMessage$).startWith("").do(x => console.log("changed"));
+        .do(x => console.log("personCountChangedMessage$: " + x));
+    let clearMessage$ = personCountChangedMessage$.flatMap(_ => Observable.timer(2000)).map(x => "").do(x => console.log("clearMessage$"));
+    let message$ = Observable.merge(personCountChangedMessage$, clearMessage$).startWith("").do(x => console.log("message$: '" + x + "'"));
 
-    let createClick$: Observable<MouseEvent> = drivers.DOM.select(".create-default").events("click");  // Observe de delete click events
+    let createClick$: Observable<MouseEvent> = drivers.DOM.select(".create-default").events("click").do(x => console.log("createClick$"));  // Observe de delete click events
 
     // save person
-    let firstNameInput$: Observable<any> = drivers.DOM.select(".firstName").events("input").map(ev => ev.target.value);
-    let lastNameInput$: Observable<any> = drivers.DOM.select(".lastName").events("input").map(ev => ev.target.value);
+    let firstNameInput$: Observable<any> = drivers.DOM.select(".firstName").events("input").map(ev => ev.target.value).do(x => console.log("firstNameInput$: " + x));
+    let lastNameInput$: Observable<any> = drivers.DOM.select(".lastName").events("input").map(ev => ev.target.value).do(x => console.log("lastNameInput$: " + x));
 
     let storageRequest$ = Observable.combineLatest(firstNameInput$, lastNameInput$, (f, l) => ({ firstName: f, lastName: l }))
         // .replay(null, 1)
         .sample(createClick$)
         .filter(x => x.firstName !== "" && x.lastName !== "")
-        .map(x => new AddPerson(x.firstName, x.lastName));
+        .map(x => new AddPerson(x.firstName, x.lastName))
+        .do(x => console.log("storageRequest$: " + JSON.stringify(x)));
 
     let vtree$ = message$
         .map(message =>
@@ -55,7 +61,7 @@ export function EditPerson(drivers: { DOM: any, PersonStoreDriver: Observable<Pe
                         span(message)
                     ])
                 ])
-            ]));
+            ])).do(x => console.log("vtree$"));
     return {
         DOM: vtree$,
         PersonStoreDriver: storageRequest$
